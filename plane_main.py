6746a572a425bc158092c7b6b137c04d2a61d927
Your Name <2426315869@qq.com>
2021-08-21 19:02:50 +0800

    飞机大战（看bilibili视频做的）

--- plane_main.py
+++ plane_main.py
@@ -1,12 +1,21 @@
 import pygame
+import os
 from pygame.constants import QUIT
 from plane_sprites import *
+pygame.init()
 screen_rect = pygame.Rect(0, 0, 480, 700)
+CREATE_ENEMY_EVENT = pygame.USEREVENT
+HERO_FIRE_EVENT = pygame.USEREVENT + 1
 
 
 class PlaneGame(object):
     def create_sprites(self):
-        pass
+        background1 = Background()
+        background2 = Background(True)
+        self.back_group = pygame.sprite.Group(background1, background2)
+        self.enemy_group = pygame.sprite.Group()
+        self.hero = Hero()
+        self.hero_group = pygame.sprite.Group(self.hero)
 
     def __init__(self) -> None:
         super().__init__()
@@ -14,6 +23,9 @@ class PlaneGame(object):
         self.screen = pygame.display.set_mode(screen_rect.size)
         self.clock = pygame.time.Clock()
         self.create_sprites()
+        self.score = 0
+        pygame.time.set_timer(CREATE_ENEMY_EVENT, 1000)
+        pygame.time.set_timer(HERO_FIRE_EVENT, 500)
 
     def start_game(self):
         print("游戏开始...")
@@ -29,16 +41,60 @@ class PlaneGame(object):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 PlaneGame.game_over()
+            elif event.type == CREATE_ENEMY_EVENT:
+                print("敌机出厂")
+                # 创建敌机精灵，并添加到精灵组中
+                enemy = Enemy()
+                self.enemy_group.add(enemy)
+            elif event.type == HERO_FIRE_EVENT:
+                self.hero.fire()
+
+            # elif event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
+            #     print("向右移动")
+            # elif event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
+            #     print("向左移动")
+        keys_pressed = pygame.key.get_pressed()
+        if keys_pressed[pygame.K_RIGHT]:
+            self.hero.speed1 = 2
+            self.hero.speed2 = 0
+        elif keys_pressed[pygame.K_LEFT]:
+            self.hero.speed1 = -2
+            self.hero.speed2 = 0
+        elif keys_pressed[pygame.K_UP]:
+            self.hero.speed2 = -2
+            self.hero.speed1 = 0
+        elif keys_pressed[pygame.K_DOWN]:
+            self.hero.speed2 = 2
+            self.hero.speed1 = 0
+        else:
+            self.hero.speed1 = 0
+            self.hero.speed2 = 0
 
     def check_collide(self):
-        pass
+        flag = pygame.sprite.groupcollide(
+            self.hero.bullets, self.enemy_group, True, True)
+        if len(flag) > 0:
+            self.score += 1
+        enemies = pygame.sprite.spritecollide(
+            self.hero, self.enemy_group, True)
+        if len(enemies) > 0:
+            self.game_over()
 
     def update_sprites(self):
-        pass
+        self.back_group.update()
+        self.back_group.draw(self.screen)
+        self.enemy_group.update()
+        self.enemy_group.draw(self.screen)
+        self.hero_group.update()
+        self.hero_group.draw(self.screen)
+        self.hero.bullets.update()
+        self.hero.bullets.draw(self.screen)
 
-    @staticmethod
-    def game_over():
+    def game_over(self):
+
+        os.system("cls")
         print("游戏结束")
+        print("你一共摧毁了", self.score, "架飞机")
         pygame.quit()
         exit()
 
--- plane_sprites.py
+++ plane_sprites.py
@@ -1,9 +1,13 @@
 import pygame
+import random
+from plane_main import screen_rect
+
+# 创建定时器常量
 
 
 class GameSprite(pygame.sprite.Sprite):
 
-    def __init__(self, image_name, speed):
+    def __init__(self, image_name, speed=1):
         super().__init__()
         self.image = pygame.image.load(image_name)
         self.rect = self.image.get_rect()
@@ -11,3 +15,89 @@ class GameSprite(pygame.sprite.Sprite):
 
     def update(self):
         self.rect.y += self.speed
+
+
+class Background(GameSprite):
+    """
+    游戏背景精灵
+    """
+
+    def __init__(self, is_alt=False):
+        super().__init__(image_name="./images/background.png")
+        if is_alt:
+            self.rect.y = -self.rect.height
+
+    def update(self):
+        super().update()
+        if self.rect.y >= screen_rect.height:
+            self.rect.y = -self.rect.height
+
+
+class Enemy(GameSprite):
+
+    def __init__(self):
+        super().__init__("./images/enemy1.png")
+        self.speed = random.randint(1, 5)
+        self.rect.bottom = 0
+        self.rect.x = random.randint(0, (screen_rect.width-self.rect.width))
+
+    def update(self):
+        super().update()
+        if self.rect.y >= screen_rect.height:
+            # print("飞出屏幕，需要从精灵组中删除...")
+            self.kill()
+
+    def __del__(self):
+        # print("敌机挂了 %s" % self.rect)
+        pass
+
+
+class Hero(GameSprite):
+    """
+    英雄精灵
+    """
+
+    def __init__(self):
+        super().__init__("./images/me1.png", 0)
+        self.speed1 = 0
+        self.speed2 = 0
+        self.rect.centerx = screen_rect.centerx
+        self.rect.bottom = screen_rect.bottom - 120
+        self.bullets = pygame.sprite.Group()
+
+    def update(self):
+        if self.rect.right <= screen_rect.right and self.rect.x >= 0:
+            self.rect.x += self.speed1
+        elif self.rect.x < 0:
+            self.rect.x = 0
+        else:
+            self.rect.right = screen_rect.right
+
+        if self.rect.bottom <= screen_rect.bottom and self.rect.y >= 0:
+            self.rect.y += self.speed2
+        elif self.rect.y < 0:
+            self.rect.y = 0
+        else:
+            self.rect.bottom = screen_rect.bottom
+
+    def fire(self):
+        print("发射子弹...")
+        for i in (0, 1, 2):
+            bullet = Bullet()
+            bullet.rect.bottom = self.rect.y - i * 20
+            bullet.rect.centerx = self.rect.centerx
+            self.bullets.add(bullet)
+
+
+class Bullet(GameSprite):
+    def __init__(self):
+        super().__init__("./images/bullet1.png", -2)
+
+    def update(self):
+        super().update()
+        if self.rect.bottom < 0:
+            self.kill()
+
+    def __del__(self):
+        # print("子弹被销毁")
+        pass
